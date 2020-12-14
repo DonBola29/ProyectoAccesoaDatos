bd5022f32ab90711a85fa35cc935e2293f5ac5f3
BOLIVAR RICARDO RAMIREZ LAGOS <bolivar.ramirez@pucese.edu.ec>
2020-12-14 10:01:49 -0500

    Agregar archivos de proyecto.

---
+++ CapaDatos/PersonaDAO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapaDatos
+{
+    public class PersonaDAO
+    {
+        public static DataTable gettAll()
+        {
+          //1. deinir y configurar la conexion con el motor de BDD
+          String cadenaConexion = 
+        }
+    }
+}
